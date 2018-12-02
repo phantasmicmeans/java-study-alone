84bdb0c315ff3c15a7ebc4ec183209be4ef22a8d
phantasmicmeans <dlaak0630@naver.com>
2018-12-03 02:47:10 +0900

    immutable arratlist with jdk

--- collections/src/ArrayList/Immutable/ImmutableArrayList.java
+++ collections/src/ArrayList/Immutable/ImmutableArrayList.java
@@ -1,5 +1,27 @@
 package ArrayList.Immutable;
 
+import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class ImmutableArrayList {
+
+    //with the core JDK
+    private List<String> list = new ArrayList<>();
+
+    public ImmutableArrayList(){ list.add("practice"); }
+
+    public List<?> makeImmutableList(){
+        //JDK provides a nice way to get an unmodifiable collection out of an existing one
+        return Collections.unmodifiableList(this.list);
+    }
+
+    public List<?> givenJdk_ImmutableList(){
+        List<String> unmodifiableList = Collections.unmodifiableList(this.list);
+        unmodifiableList.add("more practice");
+        return unmodifiableList;
+    }
 
 }
