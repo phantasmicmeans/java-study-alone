4fc9a514e4eaacfa4501f853274b317f4e7bd015
phantasmicmeans <dlaak0630@naver.com>
2018-12-03 22:19:44 +0900

    immutableList with Guava little refactoring

--- collections/src/ArrayList/Immutable/ImmutableArrayListGuava.java
+++ collections/src/ArrayList/Immutable/ImmutableArrayListGuava.java
@@ -5,25 +5,28 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 
 public class ImmutableArrayListGuava {
 
     //with Guava
-    private List<String> list = new ArrayList<>();
+    private List<String> list_guava = new ArrayList<>();
 
-    public void addToList(){ list.add("practice");}
+    public void addToList_list_guava() { list_guava.add("practice"); }
 
     @Test
     public void makeImmutableListGuava(){
         /*Guava provides sililar functionally for creating its own version of immutableList*/
-        addToList();
-        this.list = ImmutableList.copyOf(this.list);
+        addToList_list_guava();
+        //this operation will actually create a copy of the original list..
+        this.list_guava = ImmutableList.copyOf(this.list_guava);
     }
 
     @Test(expected = UnsupportedOperationException.class)
     public void givenGuava_ImmutableList(){
-        List<String> unmodifiableList = this.list;
+        makeImmutableListGuava();
+        List<String> unmodifiableList = this.list_guava;
         unmodifiableList.add("more practice");
     }
+
+
 }
