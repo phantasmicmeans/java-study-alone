d8de8931726687fc5153d2f84386029ae9c99d86
phantasmicmeans <dlaak0630@naver.com>
2018-12-04 10:23:32 +0900

    add RemoveNullandDuplicates - Duplicates
    Test logic about Remove Duplicates with Plain Java, Guava, Lambdas

--- collections/src/ArrayList/RemoveNullsAndDuplicates/RemoveNullsAndDuplicates.java
+++ collections/src/ArrayList/RemoveNullsAndDuplicates/RemoveNullsAndDuplicates.java
@@ -3,9 +3,11 @@ package ArrayList.RemoveNullsAndDuplicates;
 import com.google.common.base.Predicates;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -14,7 +16,7 @@ public class RemoveNullsAndDuplicates {
 
     //withPlainJava & withGuava will modify the list element
     @Test
-    public void removeNulls_withPlainJava(){
+    public void removeNulls_withPlainJava(){ // not Immutable
         List<String> list = Lists.newArrayList(null,"str",null);
         while(list.remove(null));
 
@@ -22,7 +24,7 @@ public class RemoveNullsAndDuplicates {
     }
 
     @Test
-    public void removeNulls_withGuava(){
+    public void removeNulls_withGuava(){ // not Immutable
         List<String> list = Lists.newArrayList(null,"str",null);
         Iterables.removeIf(list, Predicates.isNull());
 
@@ -46,5 +48,31 @@ public class RemoveNullsAndDuplicates {
                                             .collect(Collectors.toList());
 
         assertEquals(2,listWithOutNull.size());
+    }
+
+    @Test
+    public void removewDuplicateds_withPlainJava(){ //use HashSet
+        List<String> list = Lists.newArrayList("str","str","str");
+        List<String> listWithOutDuplicates = Lists.newArrayList(new HashSet<>(list));
+
+        assertEquals(1,listWithOutDuplicates.size());
+    }
+
+    @Test
+    public void removeDuplicates_withGuava(){
+        List<String> list = Lists.newArrayList("str","str","str");
+        List<String> listWithOutDuplicates  = Lists.newArrayList(Sets.newHashSet(list));
+
+        assertEquals(1, listWithOutDuplicates.size());
+    }
+
+    @Test
+    public void removeDuplicates_withLambda(){
+        List<String> list = Lists.newArrayList("str","str","str");
+        List<String> listWithOutDuplicates = list.stream()
+                                                    .distinct()
+                                                    .collect(Collectors.toList());
+
+        assertEquals(1, listWithOutDuplicates.size());
     }
 }
