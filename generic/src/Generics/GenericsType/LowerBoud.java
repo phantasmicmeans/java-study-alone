3635c324e0a8e7220e64856e5282c5e1e58866f8
phantasmicmeans <dlaak0630@naver.com>
2019-03-03 18:36:44 +0900

    add generics basic

---
+++ generic/src/Generics/GenericsType/LowerBoud.java
@@ -0,0 +1,36 @@
+package Generics.GenericsType;
+
+import com.sun.javafx.css.converters.PaintConverter;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LowerBoud {
+
+    public static void addCat(List<? super Cat> catList) {
+        catList.add(new RedCat());
+    }
+
+    @Test
+    public void testLowerBoud() {
+        List<Animal> animalList = new ArrayList<>();
+        List<Cat> catList = new ArrayList<>();
+        List<RedCat> redCatList = new ArrayList<>();
+        List<Dog> dogList = new ArrayList<>();
+
+        addCat(animalList);
+        addCat(catList);
+        //addCat(redCatList);
+        //compile time error, cannot add list of subclass RedCat Of Cat Class;
+
+        //addCat(dogList);
+        //compile time error, cannot add list of subclass Dog of SuperClass Animal of cat Class
+
+    }
+}
+
+class Animal{}
+class Cat extends Animal{};
+class RedCat extends Cat{};
+class Dog extends Animal{};
